f992413def36d3862d2ce78befc3f6058cb608ec
shahin abbaszadeh <shahinaz@gmail.com>
2014-09-21 18:13:04 +0100

    added reading in JSON and storing as a JSON object

--- APTClient.java
+++ APTClient.java
@@ -1,9 +1,11 @@
-import java.net.*;
 import java.io.*;
+import java.net.*;
 import java.util.*;
+import org.json.*;
 
 public class APTClient {
-	public static void main(String [] args) throws UnknownHostException, IOException, ClassNotFoundException, InterruptedException
+	public static void main(String [] args) throws UnknownHostException, IOException, 
+	ClassNotFoundException, InterruptedException, JSONException
 	{
 //		String serverName = args[0];
 //		int port = Integer.parseInt(args[1]);
@@ -12,6 +14,7 @@ public class APTClient {
 		int port = 22800;
 		String key = "n4sa";
 		
+
 		// initial connection
 		Socket socket = new Socket(serverName, port);
 		System.out.println("Connected to " + socket.getRemoteSocketAddress());
@@ -21,16 +24,36 @@ public class APTClient {
 				new PrintWriter(socket.getOutputStream(),true);
 		out.println(key);
 
-		BufferedReader in = 
-				new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		System.out.println(in.readLine());
+		InputStream inFromServer = socket.getInputStream();
 		
+		BufferedReader in = 
+				new BufferedReader(new InputStreamReader(inFromServer));
+		System.out.println(in.readLine());
+//		in.close();
+		
+		// pull command -- *** to be internalised!
 		Scanner inputString = new Scanner(System.in);
-		System.out.println("C: Please write command for the server: ");
+		System.out.println("please write command: ");
 		String comToServer = inputString.next();
 		out.println(comToServer);
-		System.out.println(in.readLine());
+				
+		// read JSON from the InputStream
+		StringBuilder builder = new StringBuilder();
+		String line;
+		line = in.readLine();
+		builder.append(line.substring(3));
 		
+		String jsonIn = builder.toString();
+		
+		System.out.println(jsonIn);
+		
+		JSONObject json = new JSONObject(jsonIn);
+		String output = json.toString();
+		
+		System.out.println(output);
+		
+		out.close();
+		in.close();
 		socket.close();
 	}
 }
