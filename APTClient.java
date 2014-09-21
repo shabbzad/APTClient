06ac7915c01030ca00de59585be049e6d674ae1c
shahin abbaszadeh <shahinaz@gmail.com>
2014-09-21 23:17:36 +0100

    Apply the changes continuously to the main data file

--- APTClient.java
+++ APTClient.java
@@ -14,8 +14,7 @@ public class APTClient {
 		int port = 22800;
 		String key = "n4sa";
 		
-
-		// initial connection
+		// initiate connection
 		Socket socket = new Socket(serverName, port);
 		System.out.println("Connected to " + socket.getRemoteSocketAddress());
 
@@ -25,35 +24,70 @@ public class APTClient {
 		out.println(key);
 
 		InputStream inFromServer = socket.getInputStream();
-		
 		BufferedReader in = 
 				new BufferedReader(new InputStreamReader(inFromServer));
 		System.out.println(in.readLine());
 //		in.close();
 		
-		// pull command -- *** to be internalised!
-		Scanner inputString = new Scanner(System.in);
-		System.out.println("please write command: ");
-		String comToServer = inputString.next();
-		out.println(comToServer);
-				
-		// read JSON from the InputStream
-		StringBuilder builder = new StringBuilder();
-		String line;
-		line = in.readLine();
-		builder.append(line.substring(3));
+		// read in the main content of database
+		out.println("pull");
+		String jsonMn = in.readLine().substring(3);
+		JSONObject jsonMain = new JSONObject(jsonMn);
+		System.out.println(jsonMain.toString());
 		
-		String jsonIn = builder.toString();
+		//read the change snippets and implement
+		while(true){
+			String jsonCh = in.readLine().substring(3);
+			JSONObject json = new JSONObject(jsonCh);
+			
+			System.out.println(json.toString());
+			//implement the new changes to the Main JSON object
+			//I assume it is one change per entry from the server!
+			String collection = json.getString("collection");
+			int id = json.getInt("id");
+			JSONObject change = (JSONObject) json.get("change");
+			if (jsonMain.has(collection)){
+				JSONArray tempJSON = (JSONArray) jsonMain.getJSONArray(collection);
+				boolean hasID = false;
+				for (int i = 0; i < tempJSON.length(); i++) {
+					JSONObject temptempJSON = tempJSON.getJSONObject(i);
+					if(temptempJSON.has("id") == true){
+						if (temptempJSON.getInt("id") == id){
+							
+							JSONObject oJSON = new JSONObject();
+							hasID = true;
+							Iterator i1 = temptempJSON.keys();
+							Iterator i2 = change.keys();
+							String tmp_key;
+							while(i1.hasNext()) {
+							    tmp_key = (String) i1.next();
+							    oJSON.put(tmp_key, temptempJSON.get(tmp_key));
+							}
+							while(i2.hasNext()) {
+							    tmp_key = (String) i2.next();
+							    oJSON.put(tmp_key, change.get(tmp_key));
+							}
+							tempJSON.put(oJSON);						
+						}
+					}
+					
+				}
+				if(hasID == false){
+				JSONObject nJSON = change.append("id", id);
+				tempJSON.put(nJSON);
+				}
+			}
+			else{
+				JSONObject mJSON = change.append("id", id);
+				jsonMain.put(collection, mJSON);
+			}
+
+			System.out.println(jsonMain.toString());
+		}
 		
-		System.out.println(jsonIn);
-		
-		JSONObject json = new JSONObject(jsonIn);
-		String output = json.toString();
-		
-		System.out.println(output);
-		
-		out.close();
-		in.close();
-		socket.close();
+
+//		out.close();
+//		in.close();
+//		socket.close();
 	}
 }
